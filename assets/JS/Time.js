e74a49f5f95f5a9039905a295a785351b8e23997
Wesley Riley <Wesley.A.Riley@gmail.com>
2022-05-22 17:28:12 -0500

    Addition of javascript file with basic time function.  CSS updated to have classes for the time slots in the html.

--- assets/JS/Time.js
+++ assets/JS/Time.js
@@ -1,4 +1,21 @@
 var day= document.getElementById("currentDay")
-day.append(month);
+day.append(Date());
 
-var month= Date.getMonth();
+var currentTime=new Date().getHours();
+console.log(currentTime);
+
+
+if(currentTime===17){
+    var five =document.getElementById("5pmText");
+    five.classList.add("present");
+
+    var four=document.getElementById("4pmText")
+    four.classList.remove('present')
+    four.classList.add('past')
+}
+
+if(currentTime===18){
+    var five=document.getElementById("5pmText")
+    five.classList.remove('present')
+    five.classList.add('past')
+}
